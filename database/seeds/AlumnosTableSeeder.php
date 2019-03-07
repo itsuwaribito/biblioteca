9d6243273da361bb4746955643a2403e05af3829
Jairo Nava Magaña <jnava@telecomo.com.mx>
2019-03-07 12:25:35 -0600

    correccion de bug en seeder de alumnos

--- database/seeds/AlumnosTableSeeder.php
+++ database/seeds/AlumnosTableSeeder.php
@@ -11,6 +11,6 @@ class AlumnosTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(App\User::class, 100)->create();
+        $users = factory(App\Alumno::class, 100)->create();
     }
 }
