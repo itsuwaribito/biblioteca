d3a4bc72e115a3607981bfe492c9bc55067e47f6
Jairo Nava Magaña <jnava@telecomo.com.mx>
2019-03-14 12:21:36 -0600

    correcion de bug de vistas de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,4 +46,14 @@ class LoginController extends Controller
     {
         return 'username';
     }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('app');
+    }
 }
